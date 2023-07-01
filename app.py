924a5aa950cd108c08cf8bfef5fb15d2ef49ad97
Exequiel Pagliari <58015928+exequielpagliari@users.noreply.github.com>
2023-07-01 14:44:47 -0300

    Agregado WebSocket

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template ,jsonify ,request
 from flask_cors import CORS # del modulo flask_cors importar CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:@localhost/chatdb'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Mariela@localhost:3308/chatdb'
 # URI de la BBDD driver de la BD user:clave@URLBBDD/nombreBBDD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='threading', transports=['websocket'])
+socketio = SocketIO(app)
 db= SQLAlchemy(app) #crea el objeto db de la clase SQLAlquemy
 ma=Marshmallow(app) #crea el objeto ma de de la clase Marshmallow
 
@@ -45,49 +50,74 @@ class UserSchema(ma.Schema):
 user_schema=ChatSchema() # El objeto producto_schema es para traer un producto
 users_schema=ChatSchema(many=True) # El objeto productos_schema es para traer multiples registros de producto
 
-
+user_log_ok = None
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/chat')
-def chat():
-    return render_template("chat.html")
+
 
 @app.route('/loging')
 def loging():
-    return render_template("loging.html")
+    return render_template("loging.html",status='Ingrese el usuario')
 
 @app.route('/loging-form',methods=['POST'])
-def login_form():
+def login_form(status=None):
     data = request.form
-    print(data)
     user = data['user']
     password = data['password']
-    print(user)
-    print(password)
-    print(User.name.get(str(user)))
-    return render_template("index.html")
+    user_log = User.query.filter_by(name=user).first()
+    if user_log == None:
+        print('No hay usuario')
+        return render_template("loging.html",status='No existe el usuario')
+    else:
+        if user_log.password == password:
+            user_log_ok = user_log
+            print('Usuario Loggeado')
+            return render_template("/chat.html")
+        else:
+            print('Error de Password')
+            return render_template("loging.html",status='Error de Password')
+
+
 
  
 @app.route('/singup')
 def singup():
-    return render_template("singup.html")
+    return render_template("singup.html",status='Elija nombre de su perfil y contraseña')
 
 @app.route('/singup-form',methods=['POST'])
-def singup_form():
+def singup_form(status=None):
     data = request.form
     print(data)
     user = data['user']
     password = data['password']
-    print(user)
-    print(password)
-    new_user = User(user,password)
-    db.session.add(new_user)
-    db.session.commit()
-    return render_template('chat.html')
+    user_log = User.query.filter_by(name=user).first()
+    if user_log == None:
+        new_user = User(user,password)
+        db.session.add(new_user)
+        db.session.commit()
+        user_log_ok = new_user
+        return render_template('chat.html')
+    else:
+        return render_template('singup.html',status='Usuario ya registrado')
+    
+
+@app.route('/chat')
+def chat():
+    print(user_log_ok)
+    return render_template("chat.html")
+
+@socketio.on('my event')
+def handle_message(data):
+    print(data['data'])
+
+@socketio.event
+def my_custom_event(arg1, arg2, arg3):
+    print('received args: ' + arg1 + arg2 + arg3)
 
 if __name__=='__main__':
+    socketio.run(app)
     app.run(debug=True, port=5000)
